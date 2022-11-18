3cf7f803205dc58811ad187215d46492b986c95e
Peter Latham <peter.latham@laerdal.com>
2022-11-18 15:00:11 +0100

    Add tailwind styling to the src folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
